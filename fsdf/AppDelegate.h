d30a49cec903fa203aae180e200b9e734b1cf081
ngugg <imac@imacdeimac.local>
2016-08-30 21:10:24 +0800

    Initial Commit

---
+++ fsdf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fsdf
+//
+//  Created by iMac on 16/8/30.
+//  Copyright © 2016年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fsdf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fsdf
+//
+//  Created by iMac on 16/8/30.
+//  Copyright © 2016年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
